250bb55ca09591dab1d14d5936d7bf4a9fd97d18
Potter <Benjamin.Potter@student.tdsb.on.ca>
2019-11-27 14:11:59 -0500

    init

---
+++ BenjaminPotterA3.java
@@ -0,0 +1,121 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+class BenjaminPotterA3
+{
+   /* These methods should each do the appropriate
+   sort on the String array list,  which contains listSize
+   Strings.  The method should return the time the sort takes
+   in seconds, accurate to the nearest millisecond. 
+   Convert all the letters to uppercase before sorting.*/
+   public static double insertSort(String[] list, int listSize){ 
+      long startTime = System.currentTimeMillis();
+      
+      for (int top = 1; top < listSize; top++){
+         String item = list[top];
+         int i;
+         for (i = top; i > 0 && item.compareTo(list[i - 1]) < 0; i--)
+            list[i] = list[i - 1];
+         list[i] = item;
+      }
+      
+      return System.currentTimeMillis() - startTime; 
+   }
+   public static double selectSort(String[] list, int listSize){ return 0; }
+   public static double shellsort(String[] list, int listSize){ return 0; }
+   /* This method prints up to max items from the list.
+   Print each word separated by a space, with as many
+   words on each line as possible (up to 80 characters per line)*/
+   public static void printList(String[] list, int listSize, int max) {
+      for(int i = 0; i < listSize; i++) {
+         System.out.println(list[i]);
+      }
+   }
+   /* Read all the words from a file and store them in
+   list.  Return the number of words read.  Strip all
+   punctuation from the words (other than the
+   apostrophe).  Return 0 if the file doesn't exist*/
+   public static int readList(String[] list, String fileName) {
+      int wordCount = 0;
+   
+      try { 
+         FileReader fr = new FileReader(fileName);
+         BufferedReader br = new BufferedReader(fr);
+         
+         // TODO: fix end of line chars not meaning a space
+         String file = br.lines().map(
+            l -> l + " "
+         ).collect(Collectors.joining());
+
+         StringTokenizer st = new StringTokenizer(file, " 0123456789<>?:,./;'[]{}-=_+\\\"");
+         
+         while (st.hasMoreTokens()) {
+            list[wordCount] = st.nextToken().toUpperCase();
+            wordCount++;
+         }
+         
+         br.close();
+         
+      } catch (IOException ioe) {
+      
+      }  
+      
+      return wordCount;
+   }
+   
+   public static void main(String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      String[] words = new String[1500000];
+      String[] original = new String[1500000];
+      System.out.println("Enter file name");
+      String name = input.nextLine();
+      String choice;
+      int numWords = readList(words, name);
+      double insertTime = 0, selectTime = 0, shellTime = 0;
+
+      if (numWords !=0)
+      {
+         System.arraycopy(words,0, original, 0, words.length);
+         System.out.println("Enter 1 for Insertion Sort");
+         System.out.println("Enter 2 for Selection Sort");
+         System.out.println("Enter 3 for Shellsort");
+         System.out.println("Enter 4 to do all 3 sorts");
+         choice = input.nextLine();
+         if (choice.charAt(0) == '1' || choice.charAt(0) == '4')
+         {  
+            System.out.println("Here is the list (max 2000 words) after Insertion sort:");
+            insertTime = insertSort(words, numWords);
+            printList(words, numWords, 2000);
+            System.arraycopy(original,0, words, 0, words.length);
+            System.out.println("***************************");
+         }
+         if (choice.charAt(0) == '2' || choice.charAt(0) == '4')
+         {
+            System.out.println("Here is the list (max 3000 words) after Selection sort:");
+            selectTime = selectSort(words, numWords);
+            printList(words, numWords, 3000);
+            System.arraycopy(original,0, words, 0, words.length);
+            System.out.println("***************************");
+         }
+         if (choice.charAt(0) == '3' || choice.charAt(0) == '4')
+         {
+            System.out.println("Here is the list (max 4000 words) after Shellsort:");
+            shellTime = shellsort(words, numWords);
+            printList(words, numWords, 4000);
+            System.out.println("***************************");
+         }
+
+         System.out.println("Read " + numWords + " words in " + name + ".");
+         if (choice.charAt(0) == '1' || choice.charAt(0) == '4')
+            System.out.println("It took " + insertTime + " s to sort the words using insertion sort");
+         if (choice.charAt(0) == '2' || choice.charAt(0) == '4')
+            System.out.println("It took " + selectTime + " s to sort the words using selection sort");
+         if (choice.charAt(0) == '3' || choice.charAt(0) == '4')
+            System.out.println("It took " + shellTime + " s to sort the words using Shellsort");
+      }
+      else
+         System.out.println("No such file exists.");
+   }
+      
+} 
